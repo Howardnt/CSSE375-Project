fbf088e5bd4b900334a50f314ce089c9c2a325c7
Howardnt <howardnt@rose-hulman.edu>
2026-02-10 14:27:27 -0500

    Rename to CamelCaseChecker

--- src/main/java/rhit/csse/csse374/linter/domain/CamelCaseChecker.java
+++ src/main/java/rhit/csse/csse374/linter/domain/CamelCaseChecker.java
@@ -3,6 +3,6 @@ package rhit.csse.csse374.linter.domain;
 /**
  * Skeleton implementation of {@link Cursory}.
  */
-public class cursory2 implements Cursory {
+public class CamelCaseChecker implements Cursory {
 }
 
