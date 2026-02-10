adf8d5e58af990ea7513af5f454f5ba3ee8be5ba
traverjj <traverjj@rose-hulman.edu>
2026-02-10 14:28:23 -0500

    various small fixes

--- src/main/java/rhit/csse/csse374/linter/domain/StrategyPattern.java
+++ src/main/java/rhit/csse/csse374/linter/domain/StrategyPattern.java
@@ -1,8 +1,14 @@
 package rhit.csse.csse374.linter.domain;
 
+import rhit.csse.csse374.linter.data.ASMProject;
+
 /**
  * Skeleton implementation of {@link Pattern} for detecting Strategy usage/misuse.
  */
 public class StrategyPattern implements Pattern {
+    @Override
+    public CheckResult runPatternCheck(ASMProject project) {
+        return null;
+    }
 }
 
